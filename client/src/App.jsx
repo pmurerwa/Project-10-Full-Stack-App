bb7aff45804ee21e34a9a0ee568a9797793039ee
Peace Murerwa <peacemurerwa23@gmail.com>
2024-12-18 15:23:30 -0800

    #4 created Header.jsx component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,13 @@
+import Header from './components/Header';
+
+
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header />
+      <h1>Hello World</h1>
+    </>
+    
   );
 };
 
